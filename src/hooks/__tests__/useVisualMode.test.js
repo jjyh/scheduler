bb3fc839b2a4a4f32ec4fe2eb00369f626f876d0
jjyh <janejyh@gmail.com>
2023-11-25 21:54:38 -0500

    add limit to back functon - no going beyond history

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -6,8 +6,25 @@ const FIRST = "FIRST";
 const SECOND = "SECOND";
 const THIRD = "THIRD";
 
-test("useVisualMode should initialize with default value", () => {
+test("useVisualMode should return to previous mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
+
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 });
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,10 +1,25 @@
 import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  function transition(newMode){
-    setMode(newMode)
+  const [history, setHistory] = useState([initial]);
+  function transition(nextMode, replace = false) {
+    if(replace){
+      setHistory(prev => prev.slice(0, -1));
+      setHistory(prev => [...prev, nextMode]);
+      }else{
+        setHistory(prev => [...prev, nextMode]); 
+      }
+      setMode(nextMode);
   }
-  return { mode, transition };
+  //back function
+  function back() {
+    if(history.length > 1) {
+      setHistory(history.slice(0, -1));
+      setMode(history[history.length-2]);
+    }
+  }
+
+  return { mode, transition, back};
 }
 
 //As seen here, the `useVisualMode` function can take an initial argument to set the mode state. We then return an object `{ mode }`, which can also be written as `{ mode: mode }`. This lets our tests (and components) access the current value of the mode from the hook.
