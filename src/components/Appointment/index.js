f23ef55423af12325b769a9e342eb87b9fe1d2db
jjyh <janejyh@gmail.com>
2023-11-28 21:59:57 -0500

    adding form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import "components/Appointment/styles.scss";
 
 import Header from "components/Appointment/Header";
+import Form from "components/Appointment/Form";
 
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
@@ -11,21 +12,26 @@ import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       )}
+      {mode === CREATE && (
+        <Form interviewers={props.interviewers} />
+)     }
     </article>
   );
 }
