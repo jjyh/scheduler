e5170a74fa15bebcb346448d1964eb1f55ed09d7
jjyh <janejyh@gmail.com>
2023-11-28 22:03:51 -0500

    adding cancel = back function to form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -30,8 +30,10 @@ export default function Appointment(props) {
         />
       )}
       {mode === CREATE && (
-        <Form interviewers={props.interviewers} />
+        <Form interviewers={props.interviewers}
+        onCancel ={()=>back(EMPTY)} />
 )     }
+
     </article>
   );
 }
